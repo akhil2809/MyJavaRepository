30e4e2cd64b4847743ae21f28d7d3c385720c138
akhil ramteke <akhil_ramteke@persistent.com>
2018-07-26 15:38:14 +0530

    Welcome added and date display

--- HelloWorldGit.java
+++ HelloWorldGit.java
@@ -2,16 +2,17 @@ class HelloWorld
 {
 	static void display()
 	{
-		System.out.print("DATE");
+		System.out.print("DATE"+new Date());
 	}
 	public static void main(String args[])
 	{
 		System.out.println("Hello World");
 		System.out.println("Hello World 2");
-		System.out.println("Hello World 3");
+		System.out.println("Hello World 3 Weclome to GIT!!");
 
 		HelloWorld obj=new HelloWorld();
 		obj.display();
+	
 
 	}
 }
